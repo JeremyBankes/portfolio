8c33d3a0230adb0dff7ca269faf54b34778b224f
Jeremy <jeremy.bankes@gmail.com>
2022-09-26 21:26:26 -0300

    Fixed font layout shift

---
+++ source/client/main.ts
@@ -0,0 +1,9 @@
+const styles = [
+    'color: #AA30AA;',
+    'color: #DF90DF;',
+    'color: #AA30AA;',
+    'color: #606060;',
+    'color: #505050;',
+];
+
+console.log('\n%cWelcome to %cWonderland%c.\n       ~ %cJeremy %cBankes\n', ...styles);
