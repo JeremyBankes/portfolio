3a8e38180ce0a5a5ae8600a054a77c3ad490116d
Jeremy Bankes <jeremy.bankes@gmail.com>
2023-01-28 10:51:28 -0400

    Visual update

--- source/client/main.ts
+++ source/client/main.ts
@@ -1,9 +1,17 @@
+import { Wallpaper } from "./Wallpaper.js";
+
 const styles = [
-    'color: #AA30AA;',
-    'color: #DF90DF;',
-    'color: #AA30AA;',
-    'color: #606060;',
-    'color: #505050;',
+    "color: #AA30AA;",
+    "color: #DF90DF;",
+    "color: #AA30AA;",
+    "color: #606060;",
+    "color: #505050;",
 ];
 
-console.log('\n%cWelcome to %cWonderland%c.\n       ~ %cJeremy %cBankes\n', ...styles);
+console.log("\n%cWelcome to %cWonderland%c.\n       ~ %cJeremy %cBankes\n", ...styles);
+
+const canvas = document.getElementById("wallpaperCanvas");
+if (canvas instanceof HTMLCanvasElement) {
+    const wallpaper = new Wallpaper(canvas);
+    wallpaper.start();
+}
--- source/server/index.ts
+++ source/server/index.ts
@@ -1,30 +1,15 @@
-import fs from 'fs';
-import path from 'path';
-import { Request, Response, ServerApplication } from 'framework';
+import { Request, Response, ServerApplication } from "framework";
 
 async function start() {
-    const application = new ServerApplication({ port: 7100, static: 'public' });
+    const application = new ServerApplication({ host: "localhost", port: 7100, static: "public" });
 
-    application.get('*', async (request: Request, response: Response) => {
-        logRequest(request);
-        await response.sendFile('public/index.html');
+    application.get("*", async (request: Request, response: Response) => {
+        await response.sendFile("public/index.html");
     });
 
     await application.start();
 
     console.log(`Server now listening ${application.origin}.`);
-}
-
-function logRequest(request: Request) {
-    const logDirectory = 'logs';
-    const logFileName = 'requests.txt';
-    const address = 'x-forwarded-for' in request.headers ? request.headers['x-forwarded-for'] as string : request.address;
-    if (!fs.existsSync(logDirectory)) {
-        fs.mkdirSync(logDirectory, { recursive: true });
-    }
-    const line = `${address} ${request.method} ${request.path} ${JSON.stringify(request.headers)}`;
-    fs.writeFileSync(path.join(logDirectory, logFileName), line, { encoding: 'utf8', flag: 'a' });
-    console.log(line);
 }
 
 start();
