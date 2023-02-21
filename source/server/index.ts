670e30d3da2f9c9d106fcccfdcafdbb1cbb8d42d
Jeremy Bankes <jeremy.bankes@gmail.com>
2023-02-20 22:09:46 -0400

    Page speed optimizations

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,7 +1,14 @@
-import { Request, Response, ServerApplication } from "framework";
+import { Request, Response, ServerApplication, StaticFileHandler } from "framework";
 
 async function start() {
-    const application = new ServerApplication({ host: "localhost", port: 7100, static: "public" });
+    const application = new ServerApplication({ host: "localhost", port: 7100 });
+
+    const publicHandler = new StaticFileHandler("public");
+
+    application.get("*", async (request: Request, response: Response) => {
+        response.setHeader("Cache-Control", "public, max-age=86400");
+        await publicHandler.handle(request, response);
+    });
 
     application.get("*", async (request: Request, response: Response) => {
         await response.sendFile("public/index.html");
