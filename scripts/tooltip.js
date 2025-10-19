8445ad1c259583adb80415d64edd4031e31b0dc1
Jeremy Bankes <jeremy.bankes@gmail.com>
2025-10-19 15:12:56 -0300

    Fixed tooltip flickering.

--- scripts/tooltip.js
+++ scripts/tooltip.js
@@ -7,6 +7,7 @@ function showTooltip(event, stayWithinElementId) {
     if (!(anchorElement instanceof HTMLElement)) {
         return;
     }
+    anchorElement.style.cursor = "help";
     const stayWithinElement = document.getElementById(stayWithinElementId);
     const stayWithinBounds = stayWithinElement.getBoundingClientRect();
     const tooltipElements = anchorElement.getElementsByClassName("tooltip");
@@ -14,12 +15,12 @@ function showTooltip(event, stayWithinElementId) {
 
     for (const tooltipElement of tooltipElements) {
         if (tooltipElement instanceof HTMLElement) {
+            tooltipElement.classList.toggle("hidden", event.type === "mouseleave");
             const tooltipBounds = tooltipElement.getBoundingClientRect();
             const x = Math.min(event.clientX - tooltipBounds.width / 2, stayWithinBounds.right - tooltipBounds.width);
             const y = Math.max(event.clientY - tooltipBounds.height, stayWithinBounds.top);
             tooltipElement.style.left = `${x.toFixed(1)}px`;
             tooltipElement.style.top = `${y.toFixed(1)}px`;
-            tooltipElement.classList.toggle("hidden", event.type === "mouseleave");
         }
     }
 }
